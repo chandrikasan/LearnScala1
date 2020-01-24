a019fd9fb55180729dae7ece8ff5c7ff184a9558
chandrikasan <43436721+chandrikasan@users.noreply.github.com>
2020-01-24 16:16:12 +0530

    Add files via upload

---
+++ Animals.scala
@@ -0,0 +1,13 @@
+class Animals(var x:Int, var y:Int) {
+
+  var a=x
+  var b=y
+
+  println("Creating object with " + a + " and " + b)
+
+  def print(x:Int) = {
+    var y=3
+    println(" a : " + a + " b: " + b)
+  }
+
+}
